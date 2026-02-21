82f827a65bd71213c2a8151313cb3fad5ac78e55
nozomi.shibutani <nozomi.shibutani0517@gmail.com>
2026-02-21 12:52:48 +0900

    リファクタリング完了：商品一覧取得テスト

--- src/tests/Feature/Item/GetItemListTest.php
+++ src/tests/Feature/Item/GetItemListTest.php
@@ -5,78 +5,87 @@ namespace Tests\Feature\Item;
 use Tests\TestCase;
 use App\Models\Item;
 use App\Models\User;
-
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class GetItemListTest extends TestCase
 {
-    /**
-     * 商品一覧取得
-     *
-     * @return void
-     */
-    public function testGetItemList()
-    {
-        // ------------------------
-        // 全商品を取得できる
-        // 購入済み商品は「Sold」と表示される
-        // ------------------------
+    use RefreshDatabase;
 
+    /**
+     * @test
+     * 全商品を取得できる
+     */
+    public function canGetAllItems(){
+
+        // Itemテーブルに登録がないことを確認
+        $this->assertDatabaseCount('items', 0);
         // 商品を作成する
-        $activeItem = Item::factory()->create(['status' => 1]); // 出品中
-        $soldItem   = Item::factory()->create(['status' => 2]); // 売り切れ
-        $suspendedItem = Item::factory()->create(['status' => 3]); // 出品停止中
+        foreach(Item::STATUSES as $status){
+            $item = Item::factory()->create(['status' => $status]);
+            $name[$status] = $item->name;
+        }
 
         $response = $this->get(route('items.index'));
         $response->assertStatus(200);
 
-        $content = $response->getContent();
+        // 期待する表示件数
+        $this->assertEquals(
+            2,
+            Item::where('status', '!=', Item::STATUS_SUSPENDED)->count()
+        );
 
-       // 出品中
-        $this->assertStringContainsString('data-item-id="'.$activeItem->id.'"', $content);
-        // [任意] 出品中の商品は Sold 表示されない
-        $this->assertStringNotContainsString($activeItem->name . 'Sold', $content);
+        // 表示されている商品
+        $response->assertSeeText($name[Item::STATUS_ON_SALE]);
+        $response->assertSeeText($name[Item::STATUS_SOLD]);
+        // 出品停止中は非表示
+        $response->assertDontSeeText($name[Item::STATUS_SUSPENDED]);
+    }
 
-        // 売り切れ
-        $this->assertStringContainsString('data-item-id="'.$soldItem->id.'"', $content);
-        $response->assertSeeTextInOrder([$soldItem->name,'Sold']);
+    /**
+     * @test
+     * 購入済み商品は「Sold」と表示される
+     */
+    public function purchasedItemDisplaysSold()
+    {
+            $item = Item::factory()->create(['status' => item::STATUS_SOLD]);
 
-        // [任意] 出品停止
-        $this->assertStringNotContainsString('data-item-id="'.$suspendedItem->id.'"', $content);
+            $response = $this->get(route('items.index'));
+            $response->assertStatus(200);
 
-        // ------------------------
-        // 自分が出品した商品は表示されない
-        // ------------------------
+            // 売り切れ
+            $response->assertSeeText($item->name);
+            $response->assertSeeText('Sold');
 
+    }
+
+    /**
+     * @test
+     * 自分が出品した商品は表示されない
+     */
+    public function ownItemsAreNotDisplayed()
+    {
         // ユーザーを作成してログイン
         /** @var \App\Models\User $user */
         $user = User::factory()->create();
         $this->actingAs($user);
+        // ログインしているユーザーを確認
+        $this->assertAuthenticatedAs($user);
 
-        // 自分の商品をステータスごとに作成
-        $ownActive     = Item::factory()->create(['user_id' => $user->id, 'status' => 1]); // 出品中
-        $ownSold       = Item::factory()->create(['user_id' => $user->id, 'status' => 2]); // 売り切れ
-        $ownSuspended  = Item::factory()->create(['user_id' => $user->id, 'status' => 3]); // 出品停止中
+        foreach(Item::STATUSES as $status){
+            // 商品作成
+            $item = Item::factory()->create([
+            'user_id' => $user->id,
+            'status' => $status,
+        ]);
 
-        // [任意] 他のユーザーの商品も作成
-        $otherUser = User::factory()->create();
-        $otherItemActive = Item::factory()->create(['user_id' => $otherUser->id, 'status' => 1]);
-        $otherItemSold = Item::factory()->create(['user_id' => $otherUser->id, 'status' => 2]);
-        $otherItemSuspended = Item::factory()->create(['user_id' => $otherUser->id, 'status' => 3]);
+            $response = $this->get(route('items.index'));
+            $response->assertStatus(200);
 
-        // 商品一覧画面にアクセス
-        $response = $this->get(route('items.index'));
-        $response->assertStatus(200);
+            //  商品ステータスに関わらず表示されない
+            $response->assertDontSeeText($item->name);
 
-        $content = $response->getContent();
-
-        // 自分の商品は表示されない
-        $this->assertStringNotContainsString('data-item-id="'.$ownActive->id.'"', $content);
-        $this->assertStringNotContainsString('data-item-id="'.$ownSold->id.'"', $content);
-        $this->assertStringNotContainsString('data-item-id="'.$ownSuspended->id.'"', $content);
-
-        // [任意] 他のユーザーの商品は表示される
-        $this->assertStringContainsString('data-item-id="'.$otherItemActive->id.'"', $content);
-        $this->assertStringContainsString('data-item-id="'.$otherItemSold->id.'"', $content);
-        $this->assertStringNotContainsString('data-item-id="'.$otherItemSuspended->id.'"', $content);
+            // テストデータ削除
+            Item::where('id', $item->id)->delete();
+        }
     }
 }
