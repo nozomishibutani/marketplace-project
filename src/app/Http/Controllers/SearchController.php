ce3370673d1c5a73c8be130fe1ba735203b0b20c
nozomi.shibutani <nozomi.shibutani0517@gmail.com>
2026-02-25 18:13:37 +0900

    修正：マイリスト内の検索範囲

--- src/app/Http/Controllers/SearchController.php
+++ src/app/Http/Controllers/SearchController.php
@@ -4,13 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Item;
+use App\Common\Common;
+use Illuminate\Support\Facades\Auth;
 
 class SearchController extends Controller
 {
     public function index(Request $request)
     {
-        // 全ヘッダー内の検索機能
         $keyword = $request->query('keyword');
+        $tab = $request->input('tab');
+
+        if ($tab === Common::TAB_MYLIST) {
+            // マイリストの中で検索
+            if($keyword == "" || $keyword == null){
+                // 全表示
+                $items = Item::whereHas('favorites', function ($query) {
+                                $query->where('user_id', Auth::id());
+                            })
+                            ->notSuspended()
+                            ->latest()
+                            ->get(['id', 'name', 'status', 'img']);
+            } else {
+                $items = Item::whereHas('favorites', function ($query) {
+                                $query->where('user_id', Auth::id());
+                            })
+                            ->where('name', 'LIKE', "%{$keyword}%")
+                            ->notSuspended()
+                            ->latest()
+                            ->get(['id', 'name', 'status', 'img']);
+
+                session()->flash('keyword', $keyword);
+            }
+        return view('index',compact('items'));
+        }
         // 検索ワードなし
         if($keyword == "" || $keyword == null){
             $items = Item::notSuspended()->latest()->get(['id', 'name', 'status', 'img']);
@@ -18,7 +44,7 @@ class SearchController extends Controller
             $items = Item::where('name', 'LIKE', "%{$keyword}%")->notSuspended()->latest()->get(['id', 'name', 'status', 'img']);
             session()->flash('keyword', $keyword);
         }
-        return view('index',compact('items'));
+        return view('index',compact('items', 'tab'));
     }
 }
 
