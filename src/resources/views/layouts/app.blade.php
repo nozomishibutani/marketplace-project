81d71699deca43d81b664a20efd69fe86bd47354
nozomi.shibutani <nozomi.shibutani0517@gmail.com>
2026-03-06 21:01:46 +0900

    途中：ヘッダーデザイン（フォームの位置）

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,57 +6,62 @@
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     @yield('css')
-    @yield('jquery')
     @yield('title')
 </head>
 
 <body>
-    <header class="">
-        <div class="header__logo">
-            <a href="{{ route('items.index') }}" class="header__img">
-                <img src="{{ asset('/images/header_logo.png') }}" alt="ヘッダーロゴ画像">
-            </a>
-        </div>
-        <div class="header__search-form">
-            <form action="{{ route('search')}}" method="get">
-                @if(request('tab'))
-                    <input type="hidden" name="tab" value="{{ request('tab') }}">
-                @endif
-                <input type="text" class="" name="keyword" value="{{ request('keyword') ?? null }}" placeholder="なにをお探しですか？">
-                <button class="">検索</button>
-            </form>
-        </div>
-        <nav class="header__nav">
-            <ul>
-                @if (Auth::check())
-                    <form class="form" action="/logout" method="post">
-                    @csrf
-                        <button class="header-nav__button">ログアウト</button>
+    <header class="header">
+        <div class="container">
+            <div class="header__inner">
+                <div class="header__logo">
+                    <a href="{{ route('items.index') }}">
+                        <img src="{{ asset('/images/header_logo.png') }}" alt="ヘッダーロゴ画像">
+                    </a>
+                </div>
+                <div class="header__search">
+                    <form action="{{ route('search')}}" method="get" >
+                        @if(request('tab'))
+                            <input type="hidden" name="tab" value="{{ request('tab') }}">
+                        @endif
+                            <input type="text" class="search-form" name="keyword" value="{{ request('keyword') ?? null }}" placeholder="なにをお探しですか？">
+                            <button class="">検索</button>
                     </form>
-                @else
-                    <a href="/login" class="header-nav__button">ログイン</a>
-                @endif
-                <li>
-                    <a href="{{ route('profile.index') }}" class="header-nav__button">マイページ</a>
-                </li>
-                <li>
-                    <a href="{{ route('items.create') }}" class="header-nav__button">出品</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
+                </div>
+                <nav class="header__nav">
+                    <ul class="header__ul">
+                        <li>
+                            @if (Auth::check())
+                                <form class="form" action="/logout" method="post">
+                                @csrf
+                                    <button class="header-nav__button">ログアウト</button>
+                                </form>
+                            @else
+                                <a href="/login" class="header-nav__button">ログイン</a>
+                            @endif
+                        </li>
+                        <li>
+                            <a href="{{ route('profile.index') }}" class="header-nav__button">マイページ</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('items.create') }}" class="header-nav__button">出品</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
 
-    <main>
-        {{-- マイページ画面のみ表示 --}}
-        @yield('profile')
+        <main>
+            {{-- マイページ画面のみ表示 --}}
+            @yield('profile')
 
-        {{-- 商品一覧画面、マイページのみ表示 --}}
-        @yield('menu')
+            {{-- 商品一覧画面、マイページのみ表示 --}}
+            @yield('menu')
 
-        @yield('content')
-    </main>
+            @yield('content')
+        </main>
 
-    @yield('js')
+    </div>
+
 </body>
 
 </html>
