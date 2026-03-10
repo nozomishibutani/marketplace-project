b96c737ebf1d2f924efaba38a5682fc5096e9138
nozomi.shibutani <nozomi.shibutani0517@gmail.com>
2026-03-10 14:35:11 +0900

    反映漏れ：メール認証ページ（デザイン）

---
+++ src/resources/views/auth/notice.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth/base.css') }}">
+    <title>メール認証</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__container">
+                <div class="header__logo">
+                    <a href="{{ route('items.index') }}">
+                        <img src="{{ asset('/images/header_logo.png') }}" alt="ヘッダーロゴ画像">
+                    </a>
+                </div>
+            </div>
+    </header>
+
+    <main>
+        <div class="main__container">
+            <div class="auth">
+                <div class="alert">
+                    @if(session('alert'))
+                            <p class="alert__text">{!! session('alert') !!}</p>
+                    @else
+                        <p class="alert__text">登録していただいたメールアドレスに認証メールを送付しました。<br>メール認証を完了してください。</p>
+                    @endif
+                </div>
+
+            <div class="auth__link-box">
+                <a class="auth__link auth__link--btn" href="{{ route('verification.confirm')}}">
+                    認証はこちらから
+                </a>
+            </div>
+
+            <div class="auth__btn-box">
+                <form method="post" action="{{ route('verification.send') }}">
+                    @csrf
+                    <button class="auth__btn">
+                        認証メールを再送信する
+                    </button>
+                </form>
+            </div>
+        </div>
+    </main>
+
+        <!-- メール再送信完了したメッセージ-->
+        @if(session('message'))
+            <script>
+                alert("{{ session('message') }}");
+            </script>
+        @endif
+
+    </body>
+</html>
